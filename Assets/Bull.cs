4168af355fdefd6dc2355dd0887a066150189d60
Connor Russi <connorarussi@gmail.com>
2025-02-18 00:23:31 -0600

    Initial Commit

---
+++ Assets/Bull.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bull : MonoBehaviour
+{
+    public float rotateSpeed;
+    public float maxBuckRotation = 345f;
+    public float defaultBullPos = 270f;
+    public float minButtonPress = 300f;
+    public Vector3 rotate = new Vector3 (0f, 0f, 1f);
+    public  bool buckingForward;
+    public bool stayOn;
+    public bool fall;
+    GameManager gameManagerSC;
+    // Start is called before the first frame update
+    void Start()
+    {
+       buckingForward = true;
+        gameManagerSC = GameObject.Find("GameManager").GetComponent<GameManager>();
+        stayOn = true;
+        fall = false;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        if(!fall) Buck();
+           
+    }
+    void bullAnimation()
+    {
+
+        //bull buck animation if not at max rotation
+        if (gameObject.transform.eulerAngles.z < maxBuckRotation && buckingForward) gameObject.transform.Rotate(rotate * (rotateSpeed * Time.deltaTime));
+        //If we have bucked now resetting to default
+        else if (gameObject.transform.eulerAngles.z > defaultBullPos)
+        {
+            gameObject.transform.Rotate(rotate * (-rotateSpeed * Time.deltaTime));
+        }
+    }
+    void Buck()
+    {
+        //No longer bucking forward
+        if (gameObject.transform.eulerAngles.z > maxBuckRotation && buckingForward) buckingForward = false;
+        //made it back to default position so need to reset bucking and start again
+        else if (gameObject.transform.eulerAngles.z <= defaultBullPos && !buckingForward)
+        {  
+            buckingForward = true;
+            gameManagerSC.buttonPressed = false;
+        }
+        bullAnimation();
+
+
+        //if time for button iteraction turn on logic to stay on handled by game manager
+        if (gameObject.transform.eulerAngles.z > minButtonPress && buckingForward && !gameManagerSC.buttonPressed)
+        {
+            stayOn = false;
+        }
+
+
+        //logic to see if missed timing
+        if (!buckingForward && !gameManagerSC.buttonPressed && !stayOn)
+        {
+            gameManagerSC.GameOver();
+        }
+
+
+
+    }
+}
---
+++ Assets/GameManager.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    Bull bullSC;
+    public GameObject button;
+    public GameObject cowboy;
+    public bool buttonPressed;
+    public int points;
+    public float speedUpMult;
+    public float growthStart;
+    public int bucks = 0;
+    
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        bullSC = GameObject.Find("Bull").GetComponent<Bull>();
+        button.SetActive(false);
+        buttonPressed = false;
+        points = 0;
+        bucks = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!bullSC.stayOn) StayOn();
+    }
+    public void StayOn()
+    {
+        //Debug.Log("stayON");
+        if (!bullSC.stayOn && !buttonPressed)
+        {
+            button.gameObject.SetActive(true);
+            checkButtonPressed(KeyCode.Space);
+        }
+        
+         
+        
+    }
+    void checkButtonPressed(KeyCode keyCode)
+    {
+        if (Input.anyKeyDown  && !bullSC.fall) {
+            if (Input.GetKeyDown(keyCode))
+            {
+                bullSC.stayOn = true;
+                button.SetActive(false);
+                buttonPressed = true;
+                bucks++;
+                points += Mathf.FloorToInt(bullSC.rotateSpeed);
+                bullSC.rotateSpeed += (growthStart * speedUpMult);
+                growthStart *= speedUpMult;
+                
+            }
+            else if (!Input.GetKeyDown(keyCode)) GameOver();
+        }
+        
+    }
+    public void GameOver()
+    {
+        bullSC.fall = true;
+        cowboy.transform.position = Vector3.zero;
+        Debug.Log("Earned " + points + " Points");
+    }
+}
