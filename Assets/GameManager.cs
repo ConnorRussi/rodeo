fa6fd88fc33da150acecc2f127c3213f0fb4ddd1
Connor Russi <connorarussi@gmail.com>
2025-02-19 09:46:34 -0600

    Interact buttons takes multiple possible buttons and now works/updates everything with chosen key

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,7 +1,11 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using TMPro;
+using System.Linq;
+using Random = UnityEngine.Random;
 
 public class GameManager : MonoBehaviour
 {
@@ -9,28 +13,54 @@ public class GameManager : MonoBehaviour
     public GameObject button;
     public GameObject cowboy;
     public bool buttonPressed;
+    public GameObject buttonTextGO;
+
+    public TextMeshProUGUI buttonText;
     public int points;
     public float speedUpMult;
     public float growthStart;
     public int bucks = 0;
-    public KeyCode[] keys;
+    private MyKey[] keys;
+    private MyKey selected;
     
+    
+    struct MyKey
+    {
+        public string keyName;
+        public KeyCode key;
 
+    }
     
     // Start is called before the first frame update
     void Start()
     {
         bullSC = GameObject.Find("Bull").GetComponent<Bull>();
+        button = GameObject.Find("keyButton");
+        buttonTextGO = GameObject.Find("buttonText");
+        buttonText = buttonTextGO.GetComponent<TextMeshProUGUI>();
         button.SetActive(false);
         buttonPressed = false;
         points = 0;
         bucks = 0;
+        keys = new MyKey[2];
+        MyKey key0 = new MyKey();
+        key0.key = KeyCode.RightArrow;
+        key0.keyName = "->";
+        keys[0] = key0;
+        MyKey key1 = new MyKey();
+        key1.key = KeyCode.LeftArrow;
+        key1.keyName = "<-";
+        keys[1] = key1;
+        selected = keys[0];
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!bullSC.stayOn) StayOn();
+
+        if (!bullSC.stayOn && !bullSC.fall) StayOn();
+        else if (!bullSC.fall) selected = keys[Random.Range(0, (keys.Length))];
     }
     public void StayOn()
     {
@@ -38,7 +68,8 @@ public class GameManager : MonoBehaviour
         if (!bullSC.stayOn && !buttonPressed)
         {
             button.gameObject.SetActive(true);
-            checkButtonPressed(KeyCode.Space);
+            buttonText.SetText(selected.keyName);
+            checkButtonPressed(selected.key);
         }
         
          
@@ -66,6 +97,8 @@ public class GameManager : MonoBehaviour
     {
         bullSC.fall = true;
         cowboy.transform.position = Vector3.zero;
+        button.SetActive(false);
         Debug.Log("Earned " + points + " Points");
+        
     }
 }
